65b485133b588e81ae7921c893f87f6d9230926b
Pranjal Saini <pranjalsaini05858@gmail.com>
2026-03-17 23:32:36 +0530

    added frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import NoteDes from './components/NoteDes'
+const App = () => {
+  return (
+    <div>
+
+      <NoteDes />
+    </div>
+  )
+}
+
+export default App
---
+++ frontend/src/components/NoteDes.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const NoteDes = () => {
+  return (
+    <div>
+        <h2 className='text-3xl p-7 m-10 block align-center items-center text-center  bold border '>Hey! nice to see you here 
+            I'm <span className='text-purple-400 font-serif ' > Pranjal. </span> <br />
+            Working on this Project. 
+        </h2>
+    </div>
+  )
+}
+
+export default NoteDes
