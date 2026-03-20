1bb84be4492f58ef5207aed5de225501935a4065
Pranjal Saini <pranjalsaini05858@gmail.com>
2026-03-20 16:21:56 +0530

    added middleware

--- backend/API/main.py
+++ backend/API/main.py
@@ -11,6 +11,25 @@ class Data(BaseModel):
 
 app = FastAPI()
 
+origins = [
+    "http://localhost:5173", 
+    "http://127.0.0.1:5173",
+    "http://localhost:3000", 
+    "http://127.0.0.1:3000",
+    "https://winesynth.vercel.app"
+
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,     
+    allow_credentials=True,
+    allow_methods=["*"],       
+    allow_headers=["*"],     
+)
+
+
+
 #home page
 @app.get("/")
 def home():
