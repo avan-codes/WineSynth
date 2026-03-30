84eba16a2e63b2dce89960d3271ebc3587e866b4
Pranjal Saini <pranjalsaini05858@gmail.com>
2026-03-31 03:56:57 +0530

    added production production url

--- backend/API/main.py
+++ backend/API/main.py
@@ -17,7 +17,7 @@ origins = [
     "http://127.0.0.1:5173",
     "http://localhost:3000", 
     "http://127.0.0.1:3000",
-    "https://winesynth.vercel.app"
+    "https://wine-synth.vercel.app"
 
 ]
 
