f88d2c5700bcc40f8207e9f09a8623d4df786af9
Pranjal Saini <pranjalsaini05858@gmail.com>
2026-03-31 03:37:57 +0530

    updated app.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,21 @@
-import React from 'react'
-import NoteDes from './components/NoteDes'
-const App = () => {
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import Model from "./pages/Model";
+import Report from "./pages/Report";
+
+export default function App() {
   return (
-    <div>
-
-      <NoteDes />
+    <div className="min-h-screen bg-zinc-950 text-white">
+      <Navbar />
+      <main className="pt-16">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/model" element={<Model />} />
+          <Route path="/report" element={<Report />} />
+        </Routes>
+      </main>
     </div>
-  )
+  );
 }
-
-export default App
